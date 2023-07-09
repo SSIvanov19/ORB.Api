7f302c7ba9df3174a23f1891c7b1494344664d6c
polinizal <polina.staneva@yahoo.com>
2023-07-09 22:59:56 +0300

    add TemplatesController

---
+++ OnlineResumeBuilder/ORB.WebHost/Controllers/TemplatesController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ORB.WebHost.Controllers
+{
+    [Route("/[controller]")]
+    [ApiController]
+    public class TemplatesController : ControllerBase
+    {
+
+        public static readonly List<Template> templates = new List<Template>()
+        {
+            new Template
+            {
+                Id = 1
+            },
+            new Template
+            {
+                Id = 2
+            }
+        };
+
+        [HttpGet]
+
+        public ActionResult<IEnumerable<Template>> GetTemplates()
+        {
+            return templates.ToList();
+        }
+
+        public class Template
+        {
+            public int Id { get; set; }
+        }
+    }
+}
